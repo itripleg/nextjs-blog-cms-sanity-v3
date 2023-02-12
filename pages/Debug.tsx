efdbe7e58e8b5b3e0952cb985abca1aad1c1bac9
Joshua Bell <joshua.bell.828@gmail.com>
2023-02-12 11:01:30 -0500

    Update Debug.tsx

--- pages/Debug.tsx
+++ pages/Debug.tsx
@@ -1,37 +1,14 @@
-import { OrbitControls, PerspectiveCamera, useGLTF } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import Header from 'components/Header'
-import HauntedHouse from 'models/HauntedHouse'
-import House from 'models/House'
-import Book from 'Moon/Book'
-import Chart from 'Moon/Chart'
-import Moon from 'Moon/Moon'
-import Retrograde from 'Moon/Retrograde'
-import dynamic from 'next/dynamic'
-import React from 'react'
+import MoonPhase from '../Moon/MoonPhase'
 
-const Clock = dynamic(() => import('components/Clock'), { ssr: false })
 type Props = {}
 
 export default function Debug({}: Props) {
-  // const house = useGLTF('/models/haunted_house.glb')
-
   return (
-    <div className="mx-auto grid h-screen max-h-screen w-full max-w-[100vh] grid-cols-2 place-items-center">
-      {/* <Header /> */}
-      {/* <Clock /> */}
-      <Chart />
-      <div className="px- col-span-2 w-full bg-gray-200">
-        <Canvas>
-          <ambientLight />
-          <Book />
-          <OrbitControls />
-          <HauntedHouse />
-          {/* <House /> */}
-        </Canvas>
+    <>
+      <div>
+        wtf
+        <MoonPhase />
       </div>
-      <Moon />
-      <Retrograde />
-    </div>
+    </>
   )
 }
