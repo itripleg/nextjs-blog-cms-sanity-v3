81717b98d0af8ebdb0dcad0a30a2d2024a878ddb
Joshua Bell <joshua.bell.828@gmail.com>
2023-02-12 20:25:48 -0500

    Update MoonPhase.tsx

--- Moon/MoonPhase.tsx
+++ Moon/MoonPhase.tsx
@@ -1,85 +1,64 @@
-import { format } from 'date-fns'
 import { motion } from 'framer-motion'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import React from 'react'
 
-const phases = [
-  { emoji: '🌑', sign: 'New Moon' },
-  { emoji: '🌒', sign: 'Waxing Crescent' },
-  { emoji: '🌓', sign: 'First Quarter' },
-  { emoji: '🌔', sign: 'Waxing Gibbous' },
-  { emoji: '🌕', sign: 'Full Moon' },
-  { emoji: '🌖', sign: 'Waning Gibbous' },
-  { emoji: '🌗', sign: 'Third Quarter' },
-  { emoji: '🌘', sign: 'Waning Crescent' },
-]
-
-const getMoonPhase = () => {
-  const now = new Date()
-  const unixDate = now.getTime() / 1000
-  const lunarDays = 29.53058770576
-  const lunarSecs = lunarDays * 24 * 60 * 60
-  const new2000 = new Date('2000-01-06 18:14').getTime() / 1000
-  const totalSecs = unixDate - new2000
-  let currentSecs = totalSecs % lunarSecs
-
-  if (currentSecs < 0) {
-    currentSecs += lunarSecs
-  }
-
-  const currentFrac = currentSecs / lunarSecs
-  const currentDays = currentFrac * lunarDays
-  const phaseIntervals = [
-    [0, 1],
-    [1, 6.38264692644],
-    [6.38264692644, 8.38264692644],
-    [8.38264692644, 13.76529385288],
-    [13.76529385288, 15.76529385288],
-    [15.76529385288, 21.14794077932],
-    [21.14794077932, 23.14794077932],
-    [23.14794077932, 28.53058770576],
-    [28.53058770576, 29.53058770576],
-  ]
-
-  for (let i = 0; i < phaseIntervals.length; i++) {
-    const [start, end] = phaseIntervals[i]
-    const tolerance = 0.01
-
-    if (Math.abs(currentDays - start) < tolerance) {
-      return i
-    }
-
-    if (currentDays >= start && currentDays < end) {
-      return i
-    }
-  }
+interface MoonPhaseProps {
+  lastNewMoon: Date
 }
 
-export default function Moon() {
-  const [date, setDate] = useState(new Date())
-  const [moonPhaseIndex, setMoonPhaseIndex] = useState(getMoonPhase())
+const MoonPhase: React.FC<MoonPhaseProps> = ({ lastNewMoon }) => {
+  const synodicMonth = 29.530588853 // length of a synodic month in days
+  const now = new Date()
+  const timeSinceLastNewMoon =
+    (now.getTime() - lastNewMoon.getTime()) / 86400000
+  const moonAge = timeSinceLastNewMoon % synodicMonth
+  const moonPhase = (moonAge / synodicMonth) * 8
 
-  useEffect(() => {
-    setMoonPhaseIndex(getMoonPhase())
-  }, [date])
+  const phases = [
+    { name: 'New Moon', emoji: '🌑' },
+    { name: 'Waxing Crescent', emoji: '🌒' },
+    { name: 'First Quarter', emoji: '🌓' },
+    { name: 'Waxing Gibbous', emoji: '🌔' },
+    { name: 'Full Moon', emoji: '🌕' },
+    { name: 'Waning Gibbous', emoji: '🌖' },
+    { name: 'Third Quarter', emoji: '🌗' },
+    { name: 'Waning Crescent', emoji: '🌘' },
+  ]
+
+  const phaseIndex = Math.floor(moonPhase)
+  const currentPhase = phases[phaseIndex]
+  let daysUntilNextFullMoon = 0
+  if (currentPhase.name !== 'Full Moon') {
+    if (phaseIndex < 4) {
+      daysUntilNextFullMoon =
+        (4 - phaseIndex) * (synodicMonth / 8) -
+        (phaseIndex * (synodicMonth / 8) - moonAge)
+    } else {
+      daysUntilNextFullMoon =
+        (4 - phaseIndex + 8) * (synodicMonth / 8) -
+        (phaseIndex * (synodicMonth / 8) - moonAge)
+    }
+  }
 
   return (
     <div className="py-2">
       <p className="">Moon Phase</p>
       <motion.div
-        initial={{ scale: 2 }}
-        animate={{ scale: 1.7 }}
+        initial={{ scale: 1.7 }}
+        animate={{ scale: 2 }}
         transition={{
           repeat: Infinity,
           duration: 2,
           repeatType: 'reverse',
-          // ease: 'easeInOut',
+          ease: 'easeInOut',
         }}
       >
-        <p className="text-4xl">{phases[moonPhaseIndex].emoji}</p>
+        <p className="text-4xl">{currentPhase.emoji}</p>
       </motion.div>
-      <p>{phases[moonPhaseIndex].sign}</p>
-      {/* <p>Today is {format(date, 'MMMM do, yyyy')}</p> */}
+      <p>{currentPhase.name} </p>
+      <p className="hidden">
+        There are {daysUntilNextFullMoon.toFixed(2)} days until the next full
+        moon.
+      </p>
       <style jsx>{`
         p {
           color: white;
@@ -89,3 +68,5 @@ export default function Moon() {
     </div>
   )
 }
+
+export default MoonPhase
