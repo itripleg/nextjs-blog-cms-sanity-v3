24310aef59cc45d565797d07429830c74a0daa82
Joshua Bell <joshua.bell.828@gmail.com>
2023-02-12 20:25:42 -0500

    FINALLY got ChatGPT to give me working code
    this is the simplest version too

--- Moon/Chart.tsx
+++ Moon/Chart.tsx
@@ -9,43 +9,35 @@ type Props = {
 }
 
 function Chart({ data, width, height }: Props) {
-  const xMax = d3.max(data, (d) => d[0])
-  const yMax = d3.max(data, (d) => d[1])
-
-  const xScale = d3
-    .scaleTime()
-    .domain([d3.min(data, (d) => d[0]), d3.max(data, (d) => d[0])!])
+  // domain is the min/max of input values
+  // range min/max of output values
+  let xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data.map((d) => d[0])))
     .range([0, width])
 
-  const yScale = d3
+  let yScale = d3
     .scaleLinear()
-    .domain([0, d3.max(data, (d) => +d[1])!])
-    .range([height, 0])
+    .domain(d3.extent(data.map((d) => d[1])))
+    .range([height, 0]) //inverted since the values flip when this scale is applied
 
-  const line = d3
+  // our line with scaled x,y values
+  let line = d3
     .line()
     .x((d) => xScale(d[0]))
     .y((d) => yScale(d[1]))
 
-  const result = line(data.map((d) => [d[0], yScale(d[1])]))
-
-  const xAxis = d3
-    .axisBottom(xScale)
-    .ticks(data.length / 2)
-    // @ts-ignore
-    .tickFormat((d) => '🌘')
-  // .tickFormat((d) => format(d, 'MMM'))
-
-  const yAxis = d3.axisLeft(yScale).ticks(10)
+  // the path data to feed into our svg
+  // @ts-ignore
+  let path = line(data)
+  // console.log(path)
 
   return (
-    <div className="chart-container" style={{ paddingBottom: '30px' }}>
-      <svg className=" bg-black/20 text-blue-500" width={width} height={height}>
-        <path d={result} fill="none" stroke="black" />
-        <g ref={(node) => d3.select(node).call(xAxis)} />
-        <g ref={(node) => d3.select(node).call(yAxis)} />
+    <>
+      <svg className="bg-gray-100" viewBox={`0 0 ${width} ${height}`}>
+        <path d={path} stroke="currentColor" fill="none" />
       </svg>
-    </div>
+    </>
   )
 }
 
