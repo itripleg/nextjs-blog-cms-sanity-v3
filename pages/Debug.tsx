53b1d5264322f6b5e7481e0e090402b0988f856d
Joshua Bell <joshua.bell.828@gmail.com>
2023-02-27 20:08:11 -0500

    Update Debug.tsx

--- pages/Debug.tsx
+++ pages/Debug.tsx
@@ -1,11 +1,34 @@
+import Chart from 'ChartWidget/Chart'
+import TradeData from 'ChartWidget/TradeData'
+import { useEffect, useState } from 'react'
+
 type Props = {}
 
-export default function Debug({}: Props) {
+export default function TradingPage() {
+  const [pair, setPair] = useState('BTCUSDT')
+  const [tradeData, setTradeData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`/api/trades?pair=${pair}`)
+      const data = await response.json()
+      setTradeData(data)
+    }
+
+    fetchData()
+  }, [pair])
+
   return (
-    <>
-      <div className="flex gap-2">
-        All systems<p className="uppercase text-green-800">green</p>.
+    <div>
+      <h1>Select a Trading Pair:</h1>
+      <div>
+        <button onClick={() => setPair('BTCUSDT')}>BTC/USDT</button>
+        <button onClick={() => setPair('ETHUSDT')}>ETH/USDT</button>
+        <button onClick={() => setPair('BNBUSDT')}>BNB/USDT</button>
       </div>
-    </>
+      <div>
+        <Chart data={tradeData} width={800} height={400} />
+      </div>
+    </div>
   )
 }
