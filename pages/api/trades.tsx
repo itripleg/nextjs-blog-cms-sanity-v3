8831e2ae309a363883d2963cbc0ec1d52fb9d495
Joshua Bell <joshua.bell.828@gmail.com>
2023-03-10 10:42:56 -0500

    Update trades.tsx
    change binance api endpoint to use US version

--- pages/api/trades.tsx
+++ pages/api/trades.tsx
@@ -1,11 +1,11 @@
 import Binance from 'binance-api-node'
 
 export default async function handler(req, res) {
-  const client = Binance()
+  const client = Binance({ httpBase: 'https://api.binance.us' })
   const { pair } = req.query
   const candleData = await client.candles({
     symbol: pair,
-    interval: '1d', // time interval for each candlestick (1 minute in this example)
+    interval: '1h', // time interval for each candlestick (1 minute in this example)
     limit: 100, // number of candlesticks to retrieve (100 in this example)
   })
   const tradeData = candleData.map((candle) => ({
