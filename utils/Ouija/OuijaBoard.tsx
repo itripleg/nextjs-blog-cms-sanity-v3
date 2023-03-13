358c2a822992ce97769b12f9b71303ccad8b3f33
Joshua Bell <joshua.bell.828@gmail.com>
2023-03-12 20:52:07 -0400

    Create OuijaBoard.tsx

---
+++ utils/Ouija/OuijaBoard.tsx
@@ -0,0 +1,18 @@
+import { useGLTF } from '@react-three/drei'
+import { motion } from 'framer-motion-3d'
+import React, { useRef } from 'react'
+
+export function OuijaBoard(props: any) {
+  const { nodes, materials } = useGLTF('/ouija_board.glb')
+  return (
+    <motion.group {...props} dispose={null}>
+      <motion.mesh
+        geometry={nodes['ouija(vender)'].geometry}
+        material={materials.defaultMat}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </motion.group>
+  )
+}
+
+useGLTF.preload('/ouija_board.glb')
